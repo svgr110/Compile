51edea6294a42cdc89f22f05d0334b3182d3f417
svgr110 <70746911+svgr110@users.noreply.github.com>
2021-04-08 12:26:05 +0800

    Update diy-1.sh

--- build/Lede_x86_64/diy-1.sh
+++ build/Lede_x86_64/diy-1.sh
@@ -12,8 +12,8 @@ git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
 git clone https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 git clone https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
 git clone https://github.com/vernesong/OpenClash package/luci-app-openclash
-svn co https://github.com/garypang13/openwrt-packages/trunk/lua-maxminddb
+
 find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
 find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
-
+svn co https://github.com/garypang13/openwrt-packages/trunk/lua-maxminddb
 git clone https://github.com/garypang13/luci-app-bypass package/luci-app-bypass
